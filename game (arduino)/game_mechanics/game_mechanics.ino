eab0da1d39c1230c2c62046d6c02ed7756c1e82f
Daniel Herdman <dbherdman@gmail.com>
2016-03-22 09:25:11 -0700

    I wrote more code than I should have without testing

--- game (arduino)/game_mechanics/game_mechanics.ino
+++ game (arduino)/game_mechanics/game_mechanics.ino
@@ -1,5 +1,10 @@
 // ==== Constants ====
 
+/* ---- hardware dependent ---- */
+const int MAX_BOARD_X = 8,
+          MAX_BOARD_Y = 8;
+/* -- end hardware dependent -- */
+
 /* ---- game ids ---- */
 const int CONNECT_FOUR = 0;
 /* -- end game ids -- */
@@ -53,6 +58,64 @@ int runGame(int gameId) {
 
 int playConnectFour() {
   //!!! gameplay logic
+  // ==== Connect Four Parameters ====
+  const int x_dim = 7, y_dim = 7;
+  // == End Connect Four Parameters ==
+  
+  // Create board
+  int **board = createBoard(x_dim, y_dim);
+  
+  // initialize as empty
+  int x, y;
+  for(x = 0; x < x_dim; x++) {
+    for(y = 0; y < y_dim; y++) {
+      board[x][y] = x * x_dim + y; //!!! initialize to nonsense for devel testing
+    }
+  }
+  
+  printBoard(x_dim, y_dim, board);
+  
+  // free malloc'd memory
+  freeBoard(x_dim, board);
+}
+
+/*
+ *  Allocate a x_dim by y_dim board to be used in a game
+ */
+int ** createBoard(int x_dim, int y_dim) {
+  int **board = (int **)malloc(x_dim * sizeof(int *));
+  int x, y; // loop indeces
+  for (x = 0; x < x_dim; x++) {
+    board[x] = (int *)malloc(x_dim * sizeof(int));
+  }
+  
+  return board;
+}
+
+/*
+ *  Free an x_dim by (irrelevant) board's allocated memory
+ */
+void freeBoard(int x_dim, int **board) {
+  int x, y;
+  for (x = 0; x < x_dim; x++) {
+    free(board[x]);
+  }
+  free(board);
+}
+
+/*
+ *  Output current state of a board
+ */ 
+void printBoard(int x_dim, int y_dim, int **board) {
+  //!!! to be changed to print to LEDs
+  //    uses serial for dev
+  int y, x;
+  for(y = 0; y < y_dim; y++){
+    for(x = 0; x < x_dim; x++) {
+      Serial.print(board[y][x]);Serial.print("\t");
+    }
+    Serial.println();
+  }
 }
 
 /*
