c18e35c2ace573652302a7479ac507e7ae20a502
Corwin Shanner <corwin.shanner@gmail.com>
2016-03-22 09:43:24 -0700

    little edits

--- game (arduino)/strip_test/strip_test.ino
+++ game (arduino)/strip_test/strip_test.ino
@@ -7,7 +7,7 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(5, PIN, NEO_GRB + NEO_KHZ800);
 
 /*brightness*/
-int brightness = 50;
+int brightness = 25;
 
 /*colours*/
 uint32_t red = strip.Color(255, 0, 0);
@@ -16,6 +16,7 @@ uint32_t blue = strip.Color(0, 0, 255);
 uint32_t magenta = strip.Color(255, 0, 255);
 uint32_t cyan = strip.Color(0, 255, 255);
 uint32_t yellow = strip.Color(255, 255, 0);
+uint32_t white = strip.Color(255, 255, 255);
 
 //uint16_t n = strip.numPixels(); -> determines number of pixels in strip
 
@@ -32,7 +33,12 @@ void loop() {
   //strip.setPixelColor(n, red, green, blue); or,
   //strip.setPixelColor(n, color);
 
-  strip.setPixelColor(0, magenta); //sets colour of first pixel
+  strip.setPixelColor(0, red); //sets colour of first pixel
+  strip.setPixelColor(1, yellow); //sets colour of first pixel
+  strip.setPixelColor(2, green); //sets colour of first pixel
+  strip.setPixelColor(3, cyan); //sets colour of first pixel
+  strip.setPixelColor(4, magenta); //sets colour of first pixel
+
   strip.show(); //'pushes' colour data to the strip
 
   strip.setBrightness(brightness); //from 0 to 255
