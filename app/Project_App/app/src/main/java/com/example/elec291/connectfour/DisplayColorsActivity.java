f2cee7c9a367c4cfe22a09c55dae1a3d2d9675be
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-23 23:32:08 -0700

    Differentiated between MultiPlayer and Single Player

--- app/Project_App/app/src/main/java/com/example/elec291/connectfour/DisplayColorsActivity.java
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/DisplayColorsActivity.java
@@ -1,31 +1,29 @@
 package com.example.elec291.connectfour;
 
 import android.graphics.Color;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.os.Bundle;
 import android.widget.TextView;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-import org.w3c.dom.Text;
-
-public class DisplayColorsActivity extends AppCompatActivity implements View.OnClickListener {
-
-    Button buttonRed, buttonBlue, buttonGreen, buttonMagenta, buttonYellow, buttonCyan;
-    TextView colorText2;
-
+/**
+ * Created by rohini on 23/03/16.
+ */
+public class DisplayColorsActivity extends AppCompatActivity implements View.OnClickListener{
+    Button buttonRed, buttonBlue, buttonGreen, buttonMagenta, buttonYellow, buttonCyan, buttonNext;
+    TextView colorText2, textView3, textView4, textView5, textView6, textView7, textView8;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_colors);
+        setContentView(R.layout.content_choose_chip_color);
 
+
+        //Shrink Size of pop-up down
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
@@ -33,9 +31,6 @@ public class DisplayColorsActivity extends AppCompatActivity implements View.OnC
         int height = dm.heightPixels;
 
         getWindow().setLayout((int) (width * 0.8), (int) (height * 0.8));
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         //initialization
         buttonRed = (Button) findViewById(R.id.buttonRed);
@@ -53,29 +48,6 @@ public class DisplayColorsActivity extends AppCompatActivity implements View.OnC
         buttonCyan.setOnClickListener(this);
 
     }
-
-    private void colorButtonClick(Button pressedButton){
-
-        //Change the color of the pressed button to Grey since it has already been picked
-        pressedButton.setBackgroundColor(Color.parseColor("#FF888888"));
-        colorText2 = (TextView) findViewById(R.id.colorText2);
-        colorText2.setText("Player 1");
-
-        RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        p.addRule(RelativeLayout.BELOW,pressedButton.getId());
-        colorText2.setLayoutParams(p);
-        //RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        //params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-        //colorText2.setLayoutParams(params);
-
-
-
-
-        //Change the text to tell user to choose a different color
-                ((TextView) findViewById(R.id.textView2)).setText("Second Color");
-
-    }
-
 
     @Override
     public void onClick(View v) {
@@ -99,5 +71,45 @@ public class DisplayColorsActivity extends AppCompatActivity implements View.OnC
                 colorButtonClick(buttonMagenta);
                 break;
         }
+    }
+
+    private void colorButtonClick(Button pressedButton){
+
+        switch (pressedButton.getId()){
+            case R.id.buttonRed:
+                pressedButton.setBackgroundColor(Color.parseColor("#FF888888"));
+                textView3 = (TextView) findViewById(R.id.textView3);
+                textView3.setVisibility(View.VISIBLE);
+                break;
+            case R.id.buttonBlue:
+                pressedButton.setBackgroundColor(Color.parseColor("#FF888888"));
+                textView5 = (TextView) findViewById(R.id.textView5);
+                textView5.setVisibility(View.VISIBLE);
+                break;
+            case R.id.buttonGreen:
+                pressedButton.setBackgroundColor(Color.parseColor("#FF888888"));
+                textView8 = (TextView) findViewById(R.id.textView8);
+                textView8.setVisibility(View.VISIBLE);
+                break;
+            case R.id.buttonMagenta:
+                pressedButton.setBackgroundColor(Color.parseColor("#FF888888"));
+                textView6 = (TextView) findViewById(R.id.textView6);
+                textView6.setVisibility(View.VISIBLE);
+                break;
+            case R.id.buttonCyan:
+                pressedButton.setBackgroundColor(Color.parseColor("#FF888888"));
+                textView4 = (TextView) findViewById(R.id.textView4);
+                textView4.setVisibility(View.VISIBLE);
+                break;
+            case R.id.buttonYellow:
+                pressedButton.setBackgroundColor(Color.parseColor("#FF888888"));
+                textView7 = (TextView) findViewById(R.id.textView7);
+                textView7.setVisibility(View.VISIBLE);
+                break;
+        }
+        //Change the text to tell user to choose a different color
+        ((TextView) findViewById(R.id.textView2)).setText("Choose Chip Color for CPU");
+
+
     }
 }
