45434dccbffa940d98aa9016b43d997d54141881
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-25 15:55:00 -0700

    added activities for connecting to wifi and Game instructions. But still need to be completed though

---
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/GameInstructions.java
@@ -0,0 +1,30 @@
+package com.example.elec291.connectfour;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by rohini on 24/03/16.
+ */
+public class GameInstructions extends Activity{
+    Button button2;
+    final Activity gameActivity = this;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_game_instructions);
+
+        button2 = (Button) findViewById(R.id.button2);
+        //button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+     gameActivity.finishActivity(0);
+
+    }
+}
