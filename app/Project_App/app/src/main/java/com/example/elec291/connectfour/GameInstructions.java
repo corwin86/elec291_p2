e38bfa58c3daa583db13c49c87e8cbfa8ad1a3a8
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-25 17:30:05 -0700

    Trying to make Game Instructions a pop-up

--- app/Project_App/app/src/main/java/com/example/elec291/connectfour/GameInstructions.java
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/GameInstructions.java
@@ -3,13 +3,14 @@ package com.example.elec291.connectfour;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 
 /**
  * Created by rohini on 24/03/16.
  */
-public class GameInstructions extends Activity{
+public class GameInstructions extends Activity implements View.OnClickListener{
     Button button2;
     final Activity gameActivity = this;
 
@@ -18,13 +19,20 @@ public class GameInstructions extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_game_instructions);
 
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.8));
+
         button2 = (Button) findViewById(R.id.button2);
-        //button2.setOnClickListener(this);
+        button2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v){
      gameActivity.finishActivity(0);
-
     }
 }
