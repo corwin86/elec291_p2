43f22a3f6eacccd428a9e2553aa452a00e5bc1b2
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-25 18:08:17 -0700

    Created UI for game board

---
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/GameBoard.java
@@ -0,0 +1,20 @@
+package com.example.elec291.connectfour;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by rohini on 25/03/16.
+ */
+public class GameBoard extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_game_board);
+
+        //initialization of buttons
+
+    }
+}
