d449a1486ca175609f96c563203c00cb6b8dfe2e
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-25 19:54:59 -0700

    App basically done except the instructions and colors and stuff

--- app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
@@ -1,17 +1,34 @@
 package com.example.elec291.connectfour;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by rohini on 24/03/16.
  */
 public class ConnectViaWifi extends AppCompatActivity {
 
+    Button buttonConnect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_connect_to_wifi);
+
+        //initialization
+        buttonConnect = (Button) findViewById(R.id.buttonConnect);
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent("com.example.elec291.connect4.GameBoard"));
+                startActivity(new Intent(ConnectViaWifi.this, GameBoard.class));
+
+            }
+        });
 
     }
 }
