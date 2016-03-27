5d43f8ea9156895466f8901c04cc6d10fe54031e
Corwin Shanner <corwin.shanner@gmail.com>
2016-03-26 17:22:47 -0700

    connect4Cascade & boardFull completed

--- game (arduino)/game_mechanics/game_mechanics.ino
+++ game (arduino)/game_mechanics/game_mechanics.ino
@@ -33,7 +33,7 @@ const int P1 = 1,          //
           P2 = 2,          // tokens used on board
           EMPTY_CELL = 0;  //
 const int X_DIM = 8,
-          Y_DIM = 8; 
+          Y_DIM = 8;
 /* -- end game parameters -- */
 
 /* ---- error states ---- */
@@ -46,12 +46,12 @@ const int FN_SUCCESS = 1,
 
 void setup() {
   Serial.begin(9600);
-  
+
   //added by ben
   strip.begin();
   strip.setBrightness(LED_BRIGHTNESS);
   strip.show();
-  
+
   startupLEDSequence();
 }
 
@@ -62,18 +62,18 @@ void loop() {
   // error handle
   switch (e) {
     case FN_SUCCESS : {
-      break;
-    }
+        break;
+      }
     case ERR_INVALID_GAMEID : {
-      Serial.print("** ERR - INVALID GAME ID in loop(): "); Serial.println(gameId);
-      break;
-    }
+        Serial.print("** ERR - INVALID GAME ID in loop(): "); Serial.println(gameId);
+        break;
+      }
     case ERR_GENERIC_FAIL : {
-      Serial.println("** ERR - GENERIC FAIL: runGame(gameId) failed");
-      break;
-    }
+        Serial.println("** ERR - GENERIC FAIL: runGame(gameId) failed");
+        break;
+      }
   }
-  
+
   //!!! need loop/replay handling -> TBD
 }
 
@@ -92,9 +92,9 @@ int runGame(int gameId) {
       }
     //!!! add cases per new game
     default : {
-      // gameId did not match any library games
-      return ERR_INVALID_GAMEID;
-    }
+        // gameId did not match any library games
+        return ERR_INVALID_GAMEID;
+      }
   }
 }
 
@@ -109,113 +109,134 @@ int playConnectFour() {
       board[y][x] = P1;
     }
   }
-  
-  while(1) {
-  for (y = 0; y < Y_DIM; y++) {
-    for (x = 0; x < X_DIM; x++) {
-      board[y][x] = P2;
+
+  while (1) {
+    for (y = 0; y < Y_DIM; y++) {
+      for (x = 0; x < X_DIM; x++) {
+        board[y][x] = P2;
+      }
     }
-  }
-  printBoard(board);
-  delay(250);
-  for (y = 0; y < Y_DIM; y++) {
-    for (x = 0; x < X_DIM; x++) {
-      board[y][x] = EMPTY_CELL;
+    printBoard(board);
+    delay(250);
+    for (y = 0; y < Y_DIM; y++) {
+      for (x = 0; x < X_DIM; x++) {
+        board[y][x] = EMPTY_CELL;
+      }
     }
+    printBoard(board);
+    delay(250);
   }
-  printBoard(board);
-  delay(250);
-  }
-  
-//  printCell(1, 1, red);
-//  printCell(4, 5, blue);
-//  printCell(2, 3, green);
-//  while(1);
-  
+
+  //  printCell(1, 1, red);
+  //  printCell(4, 5, blue);
+  //  printCell(2, 3, green);
+  //  while(1);
+
   //---- gameplay ----
   int cur_player = P1; //cur_player is one of {P1, P2}
-  while(!gameOver_connect4(board)) {
+  while (!gameOver_connect4(board)) {
     //!!! game logic not done
-    
+
     int col;
     do {
       //!!! -- take input --
       //!!! use serial for debug
-      while(!Serial.available()); //wait for serial data before parsing
+      while (!Serial.available()); //wait for serial data before parsing
       col = Serial.parseInt();
       Serial.println(col);
-      
+
       // clamp inputs to valid range
       col = col < 0 ? 0 : col >= X_DIM ? X_DIM - 1 : col;
     } while (!dropToken_connect4(board, col, cur_player));
-    
+
     printBoard(board);
-    
+
     cur_player = cur_player == P1 ? P2 : P1; //change turns
   }
   //-- end gameplay --
-  
+
   //!!! perhaps delay this call for user to confirm end game
   connect4Cascade(board); //GUI feature indicates game end, ***CLEARS BOARD***
-  
+
   // free malloc'd memory
   freeBoard(board);
-  
+
   return FN_SUCCESS; //game completed successfully
 }
 
 /*
- *  Place a player token on board in column x !!!untested
- */
+    Place a player token on board in column x !!!untested
+*/
 int dropToken_connect4(int **board, int col, int token) {
   int y = -1;
-  while(board[y + 1][col] == EMPTY_CELL) {
+  while (board[y + 1][col] == EMPTY_CELL) {
     y++;
   }
-  
-  if(y == -1) { //no empty cells in column col
+
+  if (y == -1) { //no empty cells in column col
     return 0;
   }
-  
+
   board[y][col] = token; //place token
-  
+
   return 1; //successfully dropped token
 }
 
 /*
- *  Return true if a player has won, or a draw is reached (board is full)
- */
+    Return true if a player has won, or a draw is reached (board is full)
+*/
 int gameOver_connect4(int **board) {
   return winningPlayer_connect4(board) > 0 || boardFull(board);
 }
 
 /*
- *  Graphical sequence replicating pulling bottom out of connect 4 board
- *  Modifies board: replaces all user tokens with EMPTY_CELL
- */
+    Graphical sequence replicating pulling bottom out of connect 4 board
+    Modifies board: replaces all user tokens with EMPTY_CELL
+*/
 void connect4Cascade(int **board) {
-  //!!!increment tokens down, falling out of bottom of board
+  int x, y, count;
+
+  for (count = 0; count < Y_DIM; count--) {
+    //replace all cells with the value above them
+    for (y = 0; y < 6 - count; y++) {
+      for (x = 0; x < X_DIM; x++) {
+        board[y][x] = board[y + 1][x];
+      }
+    }
+    //replaces top row with EMPTY_CELL
+    for (x = 0; x < X_DIM; y++) {
+      board[7 - count][x] = EMPTY_CELL;
+    }
+    printBoard(board);
+    delay(100);
+  }
 }
 
 /*
- *  Return player ID of winning player, 0 if none
- */ 
+    Return player ID of winning player, 0 if none
+*/
 int winningPlayer_connect4(int **board) {
   //!!! traverse board looking for 4 in a row
   return 0;
 }
 
 /*
- *  Returns 0 if board contains 1+ empty cells
- */
+    Returns 0 if board contains 1+ empty cells
+*/
 int boardFull(int **board) {
-  //!!! traverse board, if has empty cell return false
-  return 0;
+  int x, y;
+  for (y = 0; y < Y_DIM; y++) {
+    for (x = 0; x < X_DIM; x++) {
+      if (board[y][x] == EMPTY_CELL)
+        return 0;
+    }
+  }
+  return 1;
 }
 
 /*
- *  Allocate a x_dim by y_dim board to be used in a game
- */
+    Allocate a x_dim by y_dim board to be used in a game
+*/
 int ** createBoard() {
   int **board = (int **)malloc(Y_DIM * sizeof(int *));
   int x, y; // loop indeces
@@ -227,8 +248,8 @@ int ** createBoard() {
 }
 
 /*
- *  Free an x_dim by (irrelevant) board's allocated memory
- */
+    Free an x_dim by (irrelevant) board's allocated memory
+*/
 void freeBoard(int **board) {
   int y;
   for (y = 0; y < Y_DIM; y++) {
@@ -238,174 +259,174 @@ void freeBoard(int **board) {
 }
 
 /*
- *  Output current state of a board
- */
+    Output current state of a board
+*/
 void printBoard(int **board) {
   //!!! to be changed to print to LEDs
   //    uses serial for dev
   int x, y;
   for (y = 0; y < Y_DIM; y++) {
     for (x = 0; x < X_DIM; x++) {
-      #if !USE_LEDS
-        Serial.print(board[y][x]); Serial.print("\t");
-      #else
-        printCell(x, y, board[y][x] == P1 ? red : board[y][x] == P2 ? blue : off);
-      #endif
+#if !USE_LEDS
+      Serial.print(board[y][x]); Serial.print("\t");
+#else
+      printCell(x, y, board[y][x] == P1 ? red : board[y][x] == P2 ? blue : off);
+#endif
     }
-    #if !USE_LEDS
-      Serial.println();
-    #endif
-  }
-  #if !USE_LEDS
+#if !USE_LEDS
     Serial.println();
-  #endif
+#endif
+  }
+#if !USE_LEDS
+  Serial.println();
+#endif
 }
 
 /*
- * given a row,column and color light up specified led
- */
-void printCell(int x, int y, uint32_t color){
+   given a row,column and color light up specified led
+*/
+void printCell(int x, int y, uint32_t color) {
   int led = X_DIM * y + x;
   strip.setPixelColor(led, color); //sets colour of first pixel
-  strip.show(); //'pushes' colour data to the strip 
+  strip.show(); //'pushes' colour data to the strip
 }
 
 /*
- *  Displays a welcome/startup pattern on the LEDs
- */
+    Displays a welcome/startup pattern on the LEDs
+*/
 void startupLEDSequence() {
   int i = 0;
-  
+
   uint32_t square1 = blue;
   uint32_t square2 = cyan;
   uint32_t square3 = green;
   uint32_t square4 = yellow;
 
   //uint32_t[] colors = red, magenta, green, blue;
-  
-  for(i = 0; i < 10; i++) {//while(i == 0){
-    //printsquare2
-    printCell(3,3,square1);
-    printCell(4,3,square1);
-    printCell(3,4,square1);
-    printCell(4,4,square1);
-  
-    //print square 2
-    printCell(2,2,square2);
-    printCell(2,3,square2);
-    printCell(2,4,square2);
-    printCell(2,5,square2);
-    
-    printCell(3,2,square2);
-    printCell(4,2,square2);
-    printCell(3,5,square2);
-    printCell(4,5,square2);
-  
-    printCell(5,2,square2);
-    printCell(5,3,square2);
-    printCell(5,4,square2);
-    printCell(5,5,square2); 
-  
-    //print square 3
-    printCell(1,1,square3);  
-    printCell(1,2,square3);
-    printCell(1,3,square3);
-    printCell(1,4,square3);
-    printCell(1,5,square3);
-    printCell(1,6,square3);
-  
-    printCell(2,1,square3);  
-    printCell(3,1,square3);
-    printCell(4,1,square3);
-    printCell(5,1,square3);  
-    printCell(2,6,square3);  
-    printCell(3,6,square3);
-    printCell(4,6,square3);
-    printCell(5,6,square3); 
-  
-    printCell(6,1,square3);  
-    printCell(6,2,square3);
-    printCell(6,3,square3);
-    printCell(6,4,square3);
-    printCell(6,5,square3);
-    printCell(6,6,square3);
-  
-    //print square 4
-    printCell(0,0,square4);
-    printCell(0,1,square4);  
-    printCell(0,2,square4);
-    printCell(0,3,square4);
-    printCell(0,4,square4);
-    printCell(0,5,square4);
-    printCell(0,6,square4);
-    printCell(0,7,square4);
-  
-    printCell(1,0,square4);
-    printCell(2,0,square4);
-    printCell(3,0,square4);
-    printCell(4,0,square4);
-    printCell(5,0,square4);
-    printCell(6,0,square4);
-    printCell(1,7,square4);
-    printCell(2,7,square4);
-    printCell(3,7,square4);
-    printCell(4,7,square4);
-    printCell(5,7,square4);
-    printCell(6,7,square4);
-  
-    printCell(7,0,square4);
-    printCell(7,1,square4);  
-    printCell(7,2,square4);
-    printCell(7,3,square4);
-    printCell(7,4,square4);
-    printCell(7,5,square4);
-    printCell(7,6,square4);
-    printCell(7,7,square4);
 
-  //switch colors
+  for (i = 0; i < 10; i++) { //while(i == 0){
+    //printsquare2
+    printCell(3, 3, square1);
+    printCell(4, 3, square1);
+    printCell(3, 4, square1);
+    printCell(4, 4, square1);
+
+    //print square 2
+    printCell(2, 2, square2);
+    printCell(2, 3, square2);
+    printCell(2, 4, square2);
+    printCell(2, 5, square2);
+
+    printCell(3, 2, square2);
+    printCell(4, 2, square2);
+    printCell(3, 5, square2);
+    printCell(4, 5, square2);
+
+    printCell(5, 2, square2);
+    printCell(5, 3, square2);
+    printCell(5, 4, square2);
+    printCell(5, 5, square2);
+
+    //print square 3
+    printCell(1, 1, square3);
+    printCell(1, 2, square3);
+    printCell(1, 3, square3);
+    printCell(1, 4, square3);
+    printCell(1, 5, square3);
+    printCell(1, 6, square3);
+
+    printCell(2, 1, square3);
+    printCell(3, 1, square3);
+    printCell(4, 1, square3);
+    printCell(5, 1, square3);
+    printCell(2, 6, square3);
+    printCell(3, 6, square3);
+    printCell(4, 6, square3);
+    printCell(5, 6, square3);
+
+    printCell(6, 1, square3);
+    printCell(6, 2, square3);
+    printCell(6, 3, square3);
+    printCell(6, 4, square3);
+    printCell(6, 5, square3);
+    printCell(6, 6, square3);
+
+    //print square 4
+    printCell(0, 0, square4);
+    printCell(0, 1, square4);
+    printCell(0, 2, square4);
+    printCell(0, 3, square4);
+    printCell(0, 4, square4);
+    printCell(0, 5, square4);
+    printCell(0, 6, square4);
+    printCell(0, 7, square4);
+
+    printCell(1, 0, square4);
+    printCell(2, 0, square4);
+    printCell(3, 0, square4);
+    printCell(4, 0, square4);
+    printCell(5, 0, square4);
+    printCell(6, 0, square4);
+    printCell(1, 7, square4);
+    printCell(2, 7, square4);
+    printCell(3, 7, square4);
+    printCell(4, 7, square4);
+    printCell(5, 7, square4);
+    printCell(6, 7, square4);
+
+    printCell(7, 0, square4);
+    printCell(7, 1, square4);
+    printCell(7, 2, square4);
+    printCell(7, 3, square4);
+    printCell(7, 4, square4);
+    printCell(7, 5, square4);
+    printCell(7, 6, square4);
+    printCell(7, 7, square4);
+
+    //switch colors
     uint32_t temp = square1;
     square1 = square4;
     square4 = square3;
     square3 = square2;
     square2 = temp;
-  
-    delay(250);
-  }  
 
-  for(int j = 0; j<64; j++){
-    strip.setPixelColor(j,255,255,255);
+    delay(250);
+  }
+
+  for (int j = 0; j < 64; j++) {
+    strip.setPixelColor(j, 255, 255, 255);
     strip.show();
   }
 
-  printCell(2,1,blue);
-  printCell(3,1,blue);
-  printCell(4,1,blue);
-  printCell(5,1,blue); 
+  printCell(2, 1, blue);
+  printCell(3, 1, blue);
+  printCell(4, 1, blue);
+  printCell(5, 1, blue);
 
-  printCell(2,2,blue);
-  printCell(2,3,blue);
-  
-  printCell(3,3,blue);
-  printCell(5,4,blue);
-  printCell(5,5,blue);
-  printCell(4,6,blue);
-  printCell(3,6,blue);
-  printCell(2,6,blue);
+  printCell(2, 2, blue);
+  printCell(2, 3, blue);
+
+  printCell(3, 3, blue);
+  printCell(5, 4, blue);
+  printCell(5, 5, blue);
+  printCell(4, 6, blue);
+  printCell(3, 6, blue);
+  printCell(2, 6, blue);
   delay(5000);
 }
 
 /*
- * returns the first light in a row with other lights of the same color
- * x long.
- */
-void detectLinesXLong(int player, int X){
-  for(int i = 0; i < 64; i++){
-    
+   returns the first light in a row with other lights of the same color
+   x long.
+*/
+void detectLinesXLong(int player, int X) {
+  for (int i = 0; i < 64; i++) {
+
   }
 }
 
-int calculateLedPosition(int x, int y){
+int calculateLedPosition(int x, int y) {
   int led = 8 * y + x;
-  return led; 
+  return led;
 }
 
