9732c20d623966bfc7d470574167d9dd1bc69eba
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-29 11:34:34 -0700

    IT COMPILES

--- app/Project_App/app/src/main/java/com/example/elec291/connectfour/WifiConnection.java
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/WifiConnection.java
@@ -1,7 +1,34 @@
 package com.example.elec291.connectfour;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Created by rohini on 29/03/16.
  */
 public class WifiConnection {
+
+
+    public void POST(String url) throws IOException {
+        try {
+            URL connect_url = new URL(url);
+            HttpURLConnection httpConnection = (HttpURLConnection)  connect_url.openConnection();
+            InputStream in = new BufferedInputStream(httpConnection.getInputStream());
+            readStream(in);
+            finally {
+                httpConnection.disconnect();
+            }
+        }
+        catch(MalformedURLException e) {
+
+        }
+
+    }
+
+
 }
