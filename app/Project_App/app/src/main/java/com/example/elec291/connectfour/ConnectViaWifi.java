f37e2992ec74f868a3c6788bec0f446208722320
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-29 12:33:26 -0700

    Added a textview to see if a message was sent or not

--- app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
@@ -2,9 +2,11 @@ package com.example.elec291.connectfour;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -14,6 +16,7 @@ import java.io.IOException;
 public class ConnectViaWifi extends AppCompatActivity {
 
     Button buttonConnect;
+    TextView connectMessage;
     String urlToConnection = "http://192.168.43.82/";
 
     @Override
@@ -22,6 +25,7 @@ public class ConnectViaWifi extends AppCompatActivity {
         setContentView(R.layout.content_connect_to_wifi);
 
         //initialization
+        connectMessage = (TextView) findViewById(R.id.textView10);
         buttonConnect = (Button) findViewById(R.id.buttonConnect);
         buttonConnect.setOnClickListener(new View.OnClickListener() {
 
@@ -30,9 +34,12 @@ public class ConnectViaWifi extends AppCompatActivity {
                 //startActivity(new Intent("com.example.elec291.connect4.GameBoard"));
                 try {
                     WifiConnection.POST(urlToConnection);
+                    connectMessage.setText("Send Message");
                 } catch (IOException e) {
                     e.printStackTrace();
+                    connectMessage.setText("Did not send message");
                 }
+                SystemClock.sleep(1000);
                 startActivity(new Intent(ConnectViaWifi.this, GameBoard.class));
 
             }
