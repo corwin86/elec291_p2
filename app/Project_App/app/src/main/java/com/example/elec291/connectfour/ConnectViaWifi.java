490192f9b1d99ddff123354a28621234a89d8a8d
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-29 12:59:57 -0700

    trying a different method

--- app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
@@ -8,7 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by rohini on 24/03/16.
@@ -17,12 +23,33 @@ public class ConnectViaWifi extends AppCompatActivity {
 
     Button buttonConnect;
     TextView connectMessage;
+    String st;
     String urlToConnection = "http://192.168.43.82/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_connect_to_wifi);
+
+
+        try{
+            Socket s = new Socket("192.168.43.82", 80);
+
+            //outgoing stream redirect to socket
+            OutputStream out = s.getOutputStream();
+
+            PrintWriter output = new PrintWriter(out);
+            output.println("Hello Android!");
+            BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            st = input.readLine();
+            s.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         //initialization
         connectMessage = (TextView) findViewById(R.id.textView10);
@@ -32,13 +59,14 @@ public class ConnectViaWifi extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //startActivity(new Intent("com.example.elec291.connect4.GameBoard"));
-                try {
-                    String lineRead = WifiConnection.POST(urlToConnection);
-                    connectMessage.setText(lineRead);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    connectMessage.setText("Did not send message");
-                }
+
+                    //String lineRead = WifiConnection.POST(urlToConnection);
+                    connectMessage.setText(st);
+                
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    connectMessage.setText("Did not send message");
+//                }
                 SystemClock.sleep(3000);
                 startActivity(new Intent(ConnectViaWifi.this, GameBoard.class));
 
