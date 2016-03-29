ebe473d553869cabb5af1eccca64e56a2875701e
Rohini Goyal <rohinigoyal8@gmail.com>
2016-03-29 15:32:27 -0700

    asynchronous thread

--- app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
+++ app/Project_App/app/src/main/java/com/example/elec291/connectfour/ConnectViaWifi.java
@@ -1,6 +1,11 @@
 package com.example.elec291.connectfour;
 
+import android.content.AsyncQueryHandler;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +21,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.Socket;
@@ -90,22 +97,84 @@ public class ConnectViaWifi extends AppCompatActivity implements View.OnClickLis
 
     }
 
-    @Override
-    public void onClick(View v){
+    //@Override
+    public void onClick(View view){
         URL connect_url = null;
-        try {
-            connect_url = new URL(urlToConnection);
-            HttpURLConnection connection = (HttpURLConnection) connect_url.openConnection();
-            connection.setDoOutput(true);
-            connection.setChunkedStreamingMode(0);
-            OutputStream out = new BufferedOutputStream(connection.getOutputStream());
-            writeStream(out);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        startActivity(new Intent(ConnectViaWifi.this, GameBoard.class));
+            String stringUrl = "http://www.android.com/";
+            ConnectivityManager connmgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connmgr.getActiveNetworkInfo();
+            if(networkInfo != null && networkInfo.isConnected()){
+                new DownloadWebpageTask().execute(urlToConnection);
+            }
+            else{
+                System.out.println("no network connection available");
+            }
+//
+//            connect_url = new URL(urlToConnection);
+//            HttpURLConnection connection = (HttpURLConnection) connect_url.openConnection();
+//            connection.setDoOutput(true);
+//            connection.setChunkedStreamingMode(0);
+//            OutputStream out = new BufferedOutputStream(connection.getOutputStream());
+//            writeStream(out);
+
+
+        //startActivity(new Intent(ConnectViaWifi.this, GameBoard.class));
     }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String...urls){
+
+            //params comes from the execute() call
+            try{
+                return downloadUrl(urls[0]);
+            } catch(IOException e){
+                return "unable to retrieve webpage";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            System.out.println(result);
+        }
+
+    }
+
+    private String downloadUrl(String myurl) throws IOException{
+        InputStream is = null;
+        int len = 500;
+
+        try{
+            URL connect_url = connect_url = new URL(myurl);
+            HttpURLConnection connection = (HttpURLConnection) connect_url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+
+            connection.connect();
+            int resposne = connection.getResponseCode();
+            is = connection.getInputStream();
+
+            String contectAsString = readIt(is, len);
+            return contectAsString;
+        } finally{
+            if(is != null){
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException{
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
 
 //    public void POST(String url) throws IOException {
 //        URL connect_url = new URL(url);
@@ -129,10 +198,10 @@ public class ConnectViaWifi extends AppCompatActivity implements View.OnClickLis
 //        in.close();
 //        return sb.toString();
 //    }
-
-    public void writeStream(OutputStream out) throws IOException {
-        String written = "Android Connection";
-        out.write(written.getBytes());
-
-    }
+//
+//    public void writeStream(OutputStream out) throws IOException {
+//        String written = "Android Connection";
+//        out.write(written.getBytes());
+//
+//    }
 }
