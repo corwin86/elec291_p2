0e15e36218aeedd675eb4c3b0bfad57d164a56dd
r4i0b <taoxingyu@gmail.com>
2016-03-31 15:28:35 -0700

    in process of cleaning up code

--- WIFI/wifitest2/wifitest2.ino
+++ WIFI/wifitest2/wifitest2.ino
@@ -12,7 +12,7 @@
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
-                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
+                         SPI_CLOCK_DIVIDER); // you can change this clock speed
 
 #define WLAN_SSID       "SM-N900W8 0389"   // cannot be longer than 32 characters!
 #define WLAN_PASS       "asdfghjk"
@@ -20,75 +20,68 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
 #define LISTEN_PORT           80      // What TCP port to listen on for connections.  
-                                      // The HTTP protocol uses port 80 by default.
+// The HTTP protocol uses port 80 by default.
 
 #define MAX_ACTION            10      // Maximum length of the HTTP action that can be parsed.
 
 #define MAX_PATH              64      // Maximum length of the HTTP request path that can be parsed.
-                                      // There isn't much memory available so keep this short!
+// There isn't much memory available so keep this short!
 
 #define BUFFER_SIZE           MAX_ACTION + MAX_PATH + 20  // Size of buffer for incoming request data.
-                                                          // Since only the first line is parsed this
-                                                          // needs to be as large as the maximum action
-                                                          // and path plus a little for whitespace and
-                                                          // HTTP version.
+// Since only the first line is parsed this
+// needs to be as large as the maximum action
+// and path plus a little for whitespace and
+// HTTP version.
 
 #define TIMEOUT_MS            500    // Amount of time in milliseconds to wait for
-                                     // an incoming request to finish.  Don't set this
-                                     // too high or your server could be slow to respond.
+// an incoming request to finish.  Don't set this
+// too high or your server could be slow to respond.
 
 Adafruit_CC3000_Server httpServer(LISTEN_PORT);
-uint8_t buffer[BUFFER_SIZE+1];
+uint8_t buffer[BUFFER_SIZE + 1];
 int bufindex = 0;
-char action[MAX_ACTION+1];
-char path[MAX_PATH+1];
+char action[MAX_ACTION + 1];
+char path[MAX_PATH + 1];
 
 void setup(void)
 {
   Serial.begin(115200);
-  Serial.println(F("Hello, CC3000!\n")); 
 
+  //can be safely removed later
+  Serial.println(F("Hello, CC3000!\n"));
   Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
-  
+
   // Initialise the module
   Serial.println(F("\nInitializing..."));
   if (!cc3000.begin())
   {
     Serial.println(F("Couldn't begin()! Check your wiring?"));
-    while(1);
+    while (1);
   }
-  
+
+  //attempt to connect to wifi network
   Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
   if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
     Serial.println(F("Failed!"));
-    while(1);
+    while (1);
   }
-   
+
+  //can be safely removed later
   Serial.println(F("Connected!"));
-  
   Serial.println(F("Request DHCP"));
+
   while (!cc3000.checkDHCP())
   {
-    delay(100); // ToDo: Insert a DHCP timeout!
-  }  
-
-  // Display the IP address DNS, Gateway, etc.
-  while (! displayConnectionDetails()) {
-    delay(1000);
+    delay(100); 
   }
 
-  // ******************************************************
-  // You can safely remove this to save some flash memory!
-  // ******************************************************
-  Serial.println(F("\r\nNOTE: This sketch may cause problems with other sketches"));
-  Serial.println(F("since the .disconnect() function is never called, so the"));
-  Serial.println(F("AP may refuse connection requests from the CC3000 until a"));
-  Serial.println(F("timeout period passes.  This is normal behaviour since"));
-  Serial.println(F("there isn't an obvious moment to disconnect with a server.\r\n"));
-  
+  // Display the IP address DNS, Gateway, etc.
+  //  while (! displayConnectionDetails()) {
+  //    delay(1000);
+  //  }
+
   // Start listening for connections
   httpServer.begin();
-  
   Serial.println(F("Listening for connections..."));
 }
 
@@ -104,14 +97,14 @@ void loop(void)
     // Clear the incoming data buffer and point to the beginning of it.
     bufindex = 0;
     memset(&buffer, 0, sizeof(buffer));
-    
+
     // Clear action and path strings.
     memset(&action, 0, sizeof(action));
     memset(&path,   0, sizeof(path));
 
     // Set a timeout for reading all the incoming data.
     unsigned long endtime = millis() + TIMEOUT_MS;
-    
+
     // Read all the incoming data until it can be parsed or the timeout expires.
     bool parsed = false;
     while (!parsed && (millis() < endtime) && (bufindex < BUFFER_SIZE)) {
@@ -123,73 +116,79 @@ void loop(void)
 
     // Handle the request if it was parsed.
     if (parsed) {
+      //can be removed later
       Serial.println(F("Processing request"));
       Serial.print(F("Action: ")); Serial.println(action);
       Serial.print(F("Path: ")); Serial.println(path);
-      // Check the action to see if it was a GET request.
+      
+      // RESPONSE FOR GET REQUEST
       if (strcmp(action, "GET") == 0) {
-        // Respond with the path that was accessed.
+        // RESPOND TO GET REQUEST
         // First send the success response code.
         client.fastrprintln(F("HTTP/1.1 200 OK"));
-//        // Then send a few headers to identify the type of data returned and that
-//        // the connection will not be held open.
+        //        // Then send a few headers to identify the type of data returned and that
+        //        // the connection will not be held open.
         client.fastrprintln(F("Content-Type: text/plain"));
         client.fastrprintln(F("Connection: close"));
         client.fastrprintln(F("Server: Adafruit CC3000"));
-//        // Send an empty line to signal start of body.
+        //        // Send an empty line to signal start of body.
         client.fastrprintln(F(""));
         // Now send the response data.
         client.fastrprintln(F("Connection successful"));
         client.fastrprint(F("You accessed path: ")); client.fastrprintln(path);
         client.fastrprint(F("what is this shit"));
       }
-      else if(strcmp(action, "POST") == 0){
-        
+
+      // RESPONSE FOR POST REQUEST
+      else if (strcmp(action, "POST") == 0) {
+
         //Read data from post request body and store info into fields
         String data = "";
-        
-        while(client.available()){
+
+        while (client.available()) {
           //Serial.write(client.read());
           char currentChar = client.read();
           bool StartBody = false;
-          
-          if(currentChar == '\n')
+
+          if (currentChar == '\n')
             StartBody = true;
 
-          if(StartBody == true){
+          if (StartBody == true) {
             data += (String) currentChar;
           }
-          
-        }Serial.println(data);
-        
+
+        } Serial.println(data);
+
         //Send server response back to client
+        //This would be used to send stuff like game state, win alert, etc back to client
         client.fastrprintln(F("HTTP/1.1 200 OK"));
-//        client.fastrprintln(F("HTTP/1.1 405 Method Not Allowed"));
+
         client.fastrprintln(F("Content-Type: text/plain"));
         client.fastrprintln(F("Connection: close"));
         client.fastrprintln(F("Server: Adafruit CC3000"));
-//        // Send an empty line to signal start of body.
+        //        // Send an empty line to signal start of body.
         client.fastrprintln(F(""));
+
         
         client.fastrprintln(F("Connection successful"));
         client.fastrprint(F("You accessed path: ")); client.fastrprintln(path);
-       
-//          char temp[BUFFER_SIZE+1];
-//          for(int i = 0; i < sizeof(buffer); i++){
-//            temp[i] = (char) buffer[i];
-//          }
-//          String inputString = String(temp);
-//          
-//          Serial.print(inputString); Serial.println(" <-- printed message");
-          
+
+        //          char temp[BUFFER_SIZE+1];
+        //          for(int i = 0; i < sizeof(buffer); i++){
+        //            temp[i] = (char) buffer[i];
+        //          }
+        //          String inputString = String(temp);
+        //
+        //          Serial.print(inputString); Serial.println(" <-- printed message");
+
       }
       else {
-//        // Unsupported action, respond with an HTTP 405 method not allowed error.
-//        client.fastrprintln(F("HTTP/1.1 405 Method Not Allowed"));
-//        client.fastrprintln(F(""));
+        //        // Unsupported action, respond with an HTTP 405 method not allowed error.
+        //        client.fastrprintln(F("HTTP/1.1 405 Method Not Allowed"));
+        //        client.fastrprintln(F(""));
 
-          //if not "GET", then should be write
-//          Serial.println(buffer);
+        //if not "GET", then should be write
+        //          Serial.println(buffer);
       }
     }
 
@@ -217,7 +216,7 @@ bool parseRequest(uint8_t* buf, int bufSize, char* action, char* path) {
   // Check if the request ends with \r\n to signal end of first line.
   if (bufSize < 2)
     return false;
-  if (buf[bufSize-2] == '\r' && buf[bufSize-1] == '\n') {
+  if (buf[bufSize - 2] == '\r' && buf[bufSize - 1] == '\n') {
     parseFirstLine((char*)buf, action, path);
     return true;
   }
@@ -240,8 +239,8 @@ void parseFirstLine(char* line, char* action, char* path) {
 bool displayConnectionDetails(void)
 {
   uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-  
-  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+
+  if (!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
   {
     Serial.println(F("Unable to retrieve the IP Address!\r\n"));
     return false;
