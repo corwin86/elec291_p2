b7e85aff561abe1464b685dd58b88bd5b2f58cf8
r4i0b <taoxingyu@gmail.com>
2016-04-03 12:02:03 -0700

    clenup stage 1

--- game (arduino)/game_mechanics/game_mechanics.ino
+++ game (arduino)/game_mechanics/game_mechanics.ino
@@ -167,11 +167,6 @@ void setup() {
     delay(100);
   }
 
-  //   Display the IP address DNS, Gateway, etc.
-  //    while (! displayConnectionDetails()) {
-  //      delay(1000);
-  //    }
-
   // Start listening for connections
   httpServer.begin();
   Serial.println(F("Listening for connections..."));
@@ -1023,26 +1018,6 @@ void parseFirstLine(char* line, char* action, char* path) {
     strncpy(path, linepath, MAX_PATH);
 }
 
-// Tries to read the IP address and other connection details
-bool displayConnectionDetails(void) {
-  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-
-  if (!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
-  {
-    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
-    return false;
-  }
-  else
-  {
-    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
-    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
-    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
-    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
-    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
-    Serial.println();
-    return true;
-  }
-}
 //================ END SERVER FUNCTIONS ================= //
 
 
